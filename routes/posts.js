7a66778eb41460d0d69b7b2d0a9e5c01585b611a
Helene <helene@outlook.com>
2016-01-20 10:43:40 -0700

    first round done

--- routes/posts.js
+++ routes/posts.js
@@ -2,10 +2,47 @@ var express = require('express');
 var router = express.Router();
 var knex = require('../db/knex');
 
+function Posts() {
+  return knex('posts')
+};
+
+function Comments() {
+  return knex('comments')
+};
 
 router.get('/', function(req, res, next) {
   Posts().select().then(function (posts) {
     res.json({'SUCCESS': posts });
+  })
+});
+
+router.post('/', function(req, res, next) {
+  Posts().insert(req.body).then(function (posts) {
+    res.redirect('/')
+  })
+});
+
+router.get('/:id', function(req, res, next) {
+  Posts().where('id',req.params.id).first().then(function (posts) {
+    res.json({'SUCCESS': posts });
+  })
+});
+
+router.get('/:id/edit', function(req, res, next) {
+  Posts().select().then(function (posts) {
+    res.json({'SUCCESS': posts });
+  })
+});
+
+router.post('/:id', function(req, res, next) {
+  Posts().insert(req.body).then(function (posts) {
+    res.redirect('/')
+  })
+});
+
+router.post('/:id/delete', function(req, res, next) {
+  Posts().delete().where('id',req.params.id).then(function (posts) {
+    res.redirect('/')
   })
 });
 
